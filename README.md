dbd6305960a376c0fc577d617fc5fa9ba5944609
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2023-12-13 20:33:16 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# A meme generator
+
+Quick start:
+
+```
+$ yarn # npm install
+$ yarn build # npm run build
+````
+
+## Development
+
+This is another project we did at Scrimba, really fun concepts, I had the chance to revisit a ton of React features I forgot over time.
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <header className="header">
+            <img 
+                src="./public/troll-face.png" 
+                className="header--image"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">The best memes are always random</h4>
+        </header>
+    )
+}
