d179b3b86634b9a5d39487e514f9303438508092
Rafael Nacle <54647722+rafaelnacle@users.noreply.github.com>
2023-12-13 20:43:20 -0300

    fix image path

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
+import trollFaceImg from './public/troll-face.png'
+
 export default function Header() {
     return (
         <header className="header">
             <img 
-                src="./public/troll-face.png" 
+                src={trollFaceImg} 
                 className="header--image"
             />
             <h2 className="header--title">Meme Generator</h2>
